99f3ee973a0c9cb41d24ec1fab089b2fa032804d
Raznor <62889081+Raznor@users.noreply.github.com>
2022-02-25 15:42:27 +0100

    Basic layout

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 export default function App() {
   return (
-    <div className="container">
-      <h1>App component</h1>
-    </div>
+    <main className="container"></main>
   );
 };
