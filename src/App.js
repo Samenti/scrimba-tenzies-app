d29e668cd2b64709405083afdca7ff29a8aa59c6
Raznor <62889081+Raznor@users.noreply.github.com>
2022-02-25 15:42:48 +0100

    Basic layout of buttons and text

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import Die from './components/Die';
 
 export default function App() {
+  const dice = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+  const diceElements = dice.map(
+    (die, index) => <Die key={index} face={die}/>
+  );
   return (
-    <main className="container"></main>
+    <main className="container">
+      <h1 className="title-text">Tenzies</h1>
+      <p className="instruction-text">
+        Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
+      </p>
+      <div className="die-container">
+        {diceElements}
+      </div>
+      <button className="roll-btn">Roll</button>
+    </main>
   );
 };
---
+++ src/components/Die.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Die(props) {
+  return (
+    <div className="die">{props.face}</div>
+  );
+};
