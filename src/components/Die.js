95caf2421b8f6c68aa7374eb8a5e6b5b795c7fed
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2022-03-03 02:05:33 +0200

    Changed die faces to show dots instead of numbers

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import {nanoid} from 'nanoid';
 
 export default function Die(props) {
+  let dots = [];
+  for (let i = 0; i < props.face; i++) {
+    dots.push(<div key={nanoid()} className="dot"></div>);
+  }
+  const faceClass = `face-${props.face}`;
   return (
     <div 
-      className={props.isHeld ? "die held-die noselect" : "die noselect"}
+      className={
+        props.isHeld 
+        ? 
+        `die ${faceClass} held-die noselect` 
+        : 
+        `die ${faceClass} noselect`
+      }
       onClick={props.hold}
     >
-      {props.face}
+      {dots}
     </div>
   );
 };
